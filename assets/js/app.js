889a1e89fcf2b4e930a0bdbcc43c4aafd8711449
André Lages <99544874+Aklages@users.noreply.github.com>
2025-04-15 18:06:20 -0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -60,17 +60,318 @@ const dados = {
                 }
             ]
         },
+        {
+            "id": 4,
+            "destaque": false,
+            "titulo": "Hollow Knight: Silkso",
+            "descricao": "Silk",
+            "conteudo": "Song",
+            "plataformas" : "PC, Nintendo Switch, XBOX Game Pass, Playstation 5",
+            "genero": "Ação, Aventura, Plataforma, Indie",
+            "lancamento": "Setembro de 2025",
+            "desenvolvedora": "Team Cherry",
+            "editora": "Team Cherry",
+            "imagem_principal": "https://picsum.photos/1800/1000?random=3",
+            "imagens_complementares": [
+                {
+                    "id": 1,
+                    "src": "",
+                    "descricao": ""
+                }
+            ]
+        },
+        {
+            "id": 5,
+            "destaque": false,
+            "titulo": "Hollow Knight: Silkso",
+            "descricao": "Silk",
+            "conteudo": "Song",
+            "plataformas" : "PC, Nintendo Switch, XBOX Game Pass, Playstation 5",
+            "genero": "Ação, Aventura, Plataforma, Indie",
+            "lancamento": "Setembro de 2025",
+            "desenvolvedora": "Team Cherry",
+            "editora": "Team Cherry",
+            "imagem_principal": "https://picsum.photos/1800/600?random=3",
+            "imagens_complementares": [
+                {
+                    "id": 1,
+                    "src": "",
+                    "descricao": ""
+                }
+            ]
+        },
+        {
+            "id": 6,
+            "destaque": false,
+            "titulo": "Hollow Knight: Silkso",
+            "descricao": "Silk",
+            "conteudo": "Song",
+            "plataformas" : "PC, Nintendo Switch, XBOX Game Pass, Playstation 5",
+            "genero": "Ação, Aventura, Plataforma, Indie",
+            "lancamento": "Setembro de 2025",
+            "desenvolvedora": "Team Cherry",
+            "editora": "Team Cherry",
+            "imagem_principal": "https://picsum.photos/1800/600?random=3",
+            "imagens_complementares": [
+                {
+                    "id": 1,
+                    "src": "",
+                    "descricao": ""
+                }
+            ]
+        },
+        {
+            "id": 7,
+            "destaque": false,
+            "titulo": "Hollow Knight: Silkso",
+            "descricao": "Silk",
+            "conteudo": "Song",
+            "plataformas" : "PC, Nintendo Switch, XBOX Game Pass, Playstation 5",
+            "genero": "Ação, Aventura, Plataforma, Indie",
+            "lancamento": "Setembro de 2025",
+            "desenvolvedora": "Team Cherry",
+            "editora": "Team Cherry",
+            "imagem_principal": "https://picsum.photos/1800/600?random=3",
+            "imagens_complementares": [
+                {
+                    "id": 1,
+                    "src": "",
+                    "descricao": ""
+                }
+            ]
+        },
+        {
+            "id": 8,
+            "destaque": false,
+            "titulo": "Hollow Knight: Silkso",
+            "descricao": "Silk",
+            "conteudo": "Song",
+            "plataformas" : "PC, Nintendo Switch, XBOX Game Pass, Playstation 5",
+            "genero": "Ação, Aventura, Plataforma, Indie",
+            "lancamento": "Setembro de 2025",
+            "desenvolvedora": "Team Cherry",
+            "editora": "Team Cherry",
+            "imagem_principal": "https://picsum.photos/1800/600?random=3",
+            "imagens_complementares": [
+                {
+                    "id": 1,
+                    "src": "",
+                    "descricao": ""
+                }
+            ]
+        },
+        {
+            "id": 9,
+            "destaque": false,
+            "titulo": "Hollow Knight: Silkso",
+            "descricao": "Silk",
+            "conteudo": "Song",
+            "plataformas" : "PC, Nintendo Switch, XBOX Game Pass, Playstation 5",
+            "genero": "Ação, Aventura, Plataforma, Indie",
+            "lancamento": "Setembro de 2025",
+            "desenvolvedora": "Team Cherry",
+            "editora": "Team Cherry",
+            "imagem_principal": "https://picsum.photos/1800/600?random=3",
+            "imagens_complementares": [
+                {
+                    "id": 1,
+                    "src": "",
+                    "descricao": ""
+                }
+            ]
+        },
+        {
+            "id": 10,
+            "destaque": false,
+            "titulo": "Hollow Knight: Silkso",
+            "descricao": "Silk",
+            "conteudo": "Song",
+            "plataformas" : "PC, Nintendo Switch, XBOX Game Pass, Playstation 5",
+            "genero": "Ação, Aventura, Plataforma, Indie",
+            "lancamento": "Setembro de 2025",
+            "desenvolvedora": "Team Cherry",
+            "editora": "Team Cherry",
+            "imagem_principal": "https://picsum.photos/1800/600?random=3",
+            "imagens_complementares": [
+                {
+                    "id": 1,
+                    "src": "",
+                    "descricao": ""
+                }
+            ]
+        },
+        {
+            "id": 11,
+            "destaque": false,
+            "titulo": "Hollow Knight: Silkso",
+            "descricao": "Silk",
+            "conteudo": "Song",
+            "plataformas" : "PC, Nintendo Switch, XBOX Game Pass, Playstation 5",
+            "genero": "Ação, Aventura, Plataforma, Indie",
+            "lancamento": "Setembro de 2025",
+            "desenvolvedora": "Team Cherry",
+            "editora": "Team Cherry",
+            "imagem_principal": "https://picsum.photos/1800/600?random=3",
+            "imagens_complementares": [
+                {
+                    "id": 1,
+                    "src": "",
+                    "descricao": ""
+                }
+            ]
+        },
+        {
+            "id": 12,
+            "destaque": false,
+            "titulo": "Hollow Knight: Silkso",
+            "descricao": "Silk",
+            "conteudo": "Song",
+            "plataformas" : "PC, Nintendo Switch, XBOX Game Pass, Playstation 5",
+            "genero": "Ação, Aventura, Plataforma, Indie",
+            "lancamento": "Setembro de 2025",
+            "desenvolvedora": "Team Cherry",
+            "editora": "Team Cherry",
+            "imagem_principal": "https://picsum.photos/1800/600?random=3",
+            "imagens_complementares": [
+                {
+                    "id": 1,
+                    "src": "",
+                    "descricao": ""
+                }
+            ]
+        },
+        {
+            "id": 13,
+            "destaque": false,
+            "titulo": "Hollow Knight: Silkso",
+            "descricao": "Silk",
+            "conteudo": "Song",
+            "plataformas" : "PC, Nintendo Switch, XBOX Game Pass, Playstation 5",
+            "genero": "Ação, Aventura, Plataforma, Indie",
+            "lancamento": "Setembro de 2025",
+            "desenvolvedora": "Team Cherry",
+            "editora": "Team Cherry",
+            "imagem_principal": "https://picsum.photos/1800/600?random=3",
+            "imagens_complementares": [
+                {
+                    "id": 1,
+                    "src": "",
+                    "descricao": ""
+                }
+            ]
+        },   
+        {
+            "id": 14,
+            "destaque": false,
+            "titulo": "Hollow Knight: Silkso",
+            "descricao": "Silk",
+            "conteudo": "Song",
+            "plataformas" : "PC, Nintendo Switch, XBOX Game Pass, Playstation 5",
+            "genero": "Ação, Aventura, Plataforma, Indie",
+            "lancamento": "Setembro de 2025",
+            "desenvolvedora": "Team Cherry",
+            "editora": "Team Cherry",
+            "imagem_principal": "https://picsum.photos/1800/600?random=3",
+            "imagens_complementares": [
+                {
+                    "id": 1,
+                    "src": "",
+                    "descricao": ""
+                }
+            ]
+        },
+        {
+            "id": 15,
+            "destaque": false,
+            "titulo": "Hollow Knight: Silkso",
+            "descricao": "Silk",
+            "conteudo": "Song",
+            "plataformas" : "PC, Nintendo Switch, XBOX Game Pass, Playstation 5",
+            "genero": "Ação, Aventura, Plataforma, Indie",
+            "lancamento": "Setembro de 2025",
+            "desenvolvedora": "Team Cherry",
+            "editora": "Team Cherry",
+            "imagem_principal": "https://picsum.photos/1800/600?random=3",
+            "imagens_complementares": [
+                {
+                    "id": 1,
+                    "src": "",
+                    "descricao": ""
+                }
+            ]
+        },
+        {
+            "id": 16,
+            "destaque": false,
+            "titulo": "Hollow Knight: Silkso",
+            "descricao": "Silk",
+            "conteudo": "Song",
+            "plataformas" : "PC, Nintendo Switch, XBOX Game Pass, Playstation 5",
+            "genero": "Ação, Aventura, Plataforma, Indie",
+            "lancamento": "Setembro de 2025",
+            "desenvolvedora": "Team Cherry",
+            "editora": "Team Cherry",
+            "imagem_principal": "https://picsum.photos/1800/600?random=3",
+            "imagens_complementares": [
+                {
+                    "id": 1,
+                    "src": "",
+                    "descricao": ""
+                }
+            ]
+        },
+        {
+            "id": 17,
+            "destaque": false,
+            "titulo": "Hollow Knight: Silkso",
+            "descricao": "Silk",
+            "conteudo": "Song",
+            "plataformas" : "PC, Nintendo Switch, XBOX Game Pass, Playstation 5",
+            "genero": "Ação, Aventura, Plataforma, Indie",
+            "lancamento": "Setembro de 2025",
+            "desenvolvedora": "Team Cherry",
+            "editora": "Team Cherry",
+            "imagem_principal": "https://picsum.photos/1800/600?random=3",
+            "imagens_complementares": [
+                {
+                    "id": 1,
+                    "src": "",
+                    "descricao": ""
+                }
+            ]
+        },
+        {
+            "id": 18,
+            "destaque": false,
+            "titulo": "Hollow Knight: Silkso",
+            "descricao": "Silk",
+            "conteudo": "Song",
+            "plataformas" : "PC, Nintendo Switch, XBOX Game Pass, Playstation 5",
+            "genero": "Ação, Aventura, Plataforma, Indie",
+            "lancamento": "Setembro de 2025",
+            "desenvolvedora": "Team Cherry",
+            "editora": "Team Cherry",
+            "imagem_principal": "https://picsum.photos/1800/600?random=3",
+            "imagens_complementares": [
+                {
+                    "id": 1,
+                    "src": "",
+                    "descricao": ""
+                }
+            ]
+        },
     ]
 }
 
 const games_carousel = $("#games_carousel");
+const games_section = $("#games_section");
 
-if(games_carousel){
+if(games_carousel && games_section){
     for(i = 0; i < dados.games.length; i++){
         if(dados.games[i].destaque == true){
             if(i == 0){
                 games_carousel.append(`
-                <div class="carousel-item active">
+                <div id="${dados.games[i].id}" class="carousel-item active link">
                 <img src="${dados.games[i].imagem_principal}" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>${dados.games[i].titulo}</h5>
@@ -81,7 +382,7 @@ if(games_carousel){
             }
             else{
                 games_carousel.append(`
-                <div class="carousel-item">
+                <div id="${dados.games[i].id}" class="carousel-item link">
                 <img src="${dados.games[i].imagem_principal}" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                   <h5>${dados.games[i].titulo}</h5>
@@ -91,5 +392,29 @@ if(games_carousel){
             `);
             }
         }
+        else{
+            games_section.append(`
+            <div id="${dados.games[i].id}" class="col link">
+                <div class="card bg-danger my-3 p-3">
+                <img src="${dados.games[i].imagem_principal}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h3>${dados.games[i].titulo}</h3>
+                    <p class="card-text">${dados.games[i].descricao}</p>                    
+                </div>
+              </div>
+            </div>
+            `);
+        }
+        
+    }
+}
+
+const games_card = $(".link");
+
+if(games_card){
+    for(let i = 0; i < games_card.length; i++){
+        games_card[i].addEventListener("click", function (){
+            window.location.href = `detalhe.html?id=${games_card[i].id}`;
+        })
     }
 }
