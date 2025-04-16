3f246ec72d87a5561c80bad103e60b555a570a23
André Lages <ak1lages@gmail.com>
2025-04-16 01:02:52 -0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,14 +4,14 @@ const dados = {
             "id": 1,
             "destaque": true,
             "titulo": "Hollow Knight: Silksong",
-            "descricao": "Silk",
-            "conteudo": "Song",
-            "plataformas" : "PC, Nintendo Switch, XBOX Game Pass, Playstation 5",
+            "descricao": "Um jogo Indie de ação e aventura",
+            "conteudo": "Hollow Knight: Silksong é a sequência épica de Hollow Knight, uma aventura com insetos e heróis. Como a caçadora letal Hornet, viaje por terras totalmente novas, descubra novos poderes, lute contra vastas hordas de insetos e desvende segredos ancestrais ligados ao seu passado.",
+            "plataformas" : "PC, Nintendo Switch, Xbox Series S/X, Playstation 5",
             "genero": "Ação, Aventura, Plataforma, Indie",
             "lancamento": "Setembro de 2025",
             "desenvolvedora": "Team Cherry",
             "editora": "Team Cherry",
-            "imagem_principal": "https://picsum.photos/1800/600?random=1",
+            "imagem_principal": "./assets/img/HKS/Banner.png",
             "imagens_complementares": [
                 {
                     "id": 1,
@@ -23,14 +23,14 @@ const dados = {
         {
             "id": 2,
             "destaque": true,
-            "titulo": "Hollow Knight: Silkson",
-            "descricao": "Silk",
-            "conteudo": "Song",
-            "plataformas" : "PC, Nintendo Switch, XBOX Game Pass, Playstation 5",
-            "genero": "Ação, Aventura, Plataforma, Indie",
-            "lancamento": "Setembro de 2025",
-            "desenvolvedora": "Team Cherry",
-            "editora": "Team Cherry",
+            "titulo": "Death Stranding 2: On The Beach",
+            "descricao": "O segundo jogo da Kojima productions",
+            "conteudo": "Embarque em uma missão inspiradora de conexão humana além da UCA. Sam com seus companheiros ao seu lado parte em uma nova jornada para salvar a humanidade da extinção. Junte-se a eles enquanto atravessam um mundo assolado por inimigos sobrenaturais, obstáculos e uma pergunta inquietante: deveríamos ter nos conectado? Passo a passo, o lendário criador de jogos Hideo Kojima muda o mundo mais uma vez.",
+            "plataformas" : "PlayStation 5",
+            "genero": "Ação, Aventura",
+            "lancamento": "Junho de 2025",
+            "desenvolvedora": "Kojima Productions",
+            "editora": "Sony Interactive Entertainment",
             "imagem_principal": "https://picsum.photos/1800/600?random=2",
             "imagens_complementares": [
                 {
@@ -43,14 +43,14 @@ const dados = {
         {
             "id": 3,
             "destaque": true,
-            "titulo": "Hollow Knight: Silkso",
-            "descricao": "Silk",
-            "conteudo": "Song",
-            "plataformas" : "PC, Nintendo Switch, XBOX Game Pass, Playstation 5",
-            "genero": "Ação, Aventura, Plataforma, Indie",
-            "lancamento": "Setembro de 2025",
-            "desenvolvedora": "Team Cherry",
-            "editora": "Team Cherry",
+            "titulo": "Assassin's Creed Shadows",
+            "descricao": "Jogue o novo Assasin's Creed, finalmente a ubisoft volta a acertar nas produções!",
+            "conteudo": "Um novo Credo surge sobre o Japão. Viva as histórias entrelaçadas de Naoe, uma habilidosa Assassina shinobi da Província de Iga, e Yasuke, o poderoso samurai africano da lenda histórica. Tendo como pano de fundo o turbulento período Sengoku tardio, esta dupla notável descobrirá seu destino comum enquanto inaugura uma nova era para o Japão.",
+            "plataformas" : "PC, PlayStation 5, Xbox Series S/X",
+            "genero": "Ação, Aventura",
+            "lancamento": "Março de 2025",
+            "desenvolvedora": "Ubisoft",
+            "editora": "Ubisoft Entertainment",
             "imagem_principal": "https://picsum.photos/1800/600?random=3",
             "imagens_complementares": [
                 {
@@ -372,8 +372,8 @@ if(games_carousel && games_section){
             if(i == 0){
                 games_carousel.append(`
                 <div id="${dados.games[i].id}" class="carousel-item active link">
-                <img src="${dados.games[i].imagem_principal}" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
+                <img src="${dados.games[i].imagem_principal}" class="d-block w-100 corte rounded" alt="...">
+                <div class="carousel-caption d-block">
                   <h5>${dados.games[i].titulo}</h5>
                   <p>${dados.games[i].descricao}</p>
                 </div>
@@ -383,8 +383,8 @@ if(games_carousel && games_section){
             else{
                 games_carousel.append(`
                 <div id="${dados.games[i].id}" class="carousel-item link">
-                <img src="${dados.games[i].imagem_principal}" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
+                <img src="${dados.games[i].imagem_principal}" class="d-block w-100 corte rounded" alt="...">
+                <div class="carousel-caption d-block">
                   <h5>${dados.games[i].titulo}</h5>
                   <p>${dados.games[i].descricao}</p>
                 </div>
@@ -396,7 +396,7 @@ if(games_carousel && games_section){
             games_section.append(`
             <div id="${dados.games[i].id}" class="col link">
                 <div class="card bg-danger my-3 p-3">
-                <img src="${dados.games[i].imagem_principal}" class="card-img-top" alt="...">
+                <img src="${dados.games[i].imagem_principal}" class="card-img-top img-fluid" alt="...">
                 <div class="card-body">
                     <h3>${dados.games[i].titulo}</h3>
                     <p class="card-text">${dados.games[i].descricao}</p>                    
